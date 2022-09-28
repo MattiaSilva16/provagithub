ff75845d7becf475485123632f186cb1251d4a26
MattiaSilva16 <mattia.silva.studenti@isii.it>
2022-09-28 08:28:30 +0200

    sui

--- provagithub/Form1.cs
+++ provagithub/Form1.cs
@@ -11,5 +11,10 @@ namespace provagithub
         {
             
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("sui");
+        }
     }
 }
